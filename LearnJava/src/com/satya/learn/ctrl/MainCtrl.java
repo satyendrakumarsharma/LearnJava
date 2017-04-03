17c0707e1c0ab424cc0bfc1de364978a210b1a9e
703162557 <703162557@BSEZ1F485D.IND.CORP.AD>
2017-04-03 13:19:45 +0530

    Learning main method.

--- LearnJava/src/com/satya/learn/ctrl/MainCtrl.java
+++ LearnJava/src/com/satya/learn/ctrl/MainCtrl.java
@@ -1,9 +1,33 @@
 package com.satya.learn.ctrl;
 
-public class MainCtrl {
+class MainCtrl {
+
+	private static int ctr = 0;
+
+	private MainCtrl() {
+		System.out.println("private constructor of MainCtrl");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("MainCtrl");
+		MainCtrl mc = new MainCtrl();
+		mc.printer();
+		mc.printer();
 	}
 
+	private void printer() {
+		try {
+			super.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		System.out.println("Counter :: " + ctr++);
+	}
+}
+
+class Satya {
+
+	public static void main(String[] args) {
+		System.out.println("Satya....");
+	}
 }
